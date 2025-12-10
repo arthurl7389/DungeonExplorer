50f36e19c474d60bddcc8c0c6029efd0bacf0234
Pierrick Causeret <pierrick.causeret@gmail.com>
2025-12-10 23:02:59 +0100

    change speed because les mods ils vont trop viteeee

--- Applications/DungeonExplorer/DungeonExplorer.h
+++ Applications/DungeonExplorer/DungeonExplorer.h
@@ -21,7 +21,7 @@ class DungeonExplorer {
 	EcranBochs *ecran;
 	ui16_t WIDTH;
 	ui16_t HEIGHT;
-	const char SPEED = 10;
+	const char SPEED = 1;
 	Player player1;
 	Player player2;
 	int mobCount = 2;
